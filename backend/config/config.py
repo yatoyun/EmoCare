923523d1b0637a83278891ca230e8b296bd84109
yatoyun <kaneko.yuta0813@gmail.com>
2025-01-12 20:44:31 +0900

    [infra] fix CLIENT_HOST env

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,25 +2,21 @@ import os
 
 STAGE = os.getenv("STAGE")
 SECRET_KEY = os.getenv("SECRET_KEY")
+client_host = os.getenv("CLIENT_HOST")
 
 class _Config:
     def __init__(self):
         self.STAGE = STAGE
         self.SECRET_KEY = SECRET_KEY
+        self.ALLOWED_HOSTS = [client_host.split("//")[1].split(":")[0]]
+        self.CORS_ALLOWED_ORIGINS = [client_host]
+        self.CORS_ORIGIN_WHITELIST = [client_host]
+        self.CSRF_TRUSTED_ORIGINS = [client_host]
         if STAGE == "local":
             self.DEBUG = True
-            local_host = "http://localhost:5173"
-            self.ALLOWED_HOSTS = ["localhost","127.0.0.1"]
-            self.CORS_ALLOWED_ORIGINS = [local_host]
-            self.CORS_ORIGIN_WHITELIST = [local_host]
-            self.CSRF_TRUSTED_ORIGINS = [local_host]
         else:
             # production
             self.DEBUG = False
-            client_host = os.getenv("CLIENT_HOST")
-            self.ALLOWED_HOSTS = [client_host.split("//")[1]]
-            self.CORS_ALLOWED_ORIGINS = [client_host]
-            self.CORS_ORIGIN_WHITELIST = [client_host]
-            self.CSRF_TRUSTED_ORIGINS = [client_host]
+
 
 config = _Config()
