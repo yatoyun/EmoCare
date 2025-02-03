736f62611b04c8f21da2d2618b07765291da368b
yatoyun <kaneko.yuta0813@gmail.com>
2025-02-03 18:33:08 +0900

    [frontend] add drawer

---
+++ frontend/src/components/ui/drawer.jsx
@@ -0,0 +1,143 @@
+import * as DrawerPrimitive from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import { cva } from 'class-variance-authority';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import { cn } from '@/utils/cn';
+
+const Drawer = DrawerPrimitive.Root;
+
+const DrawerTrigger = DrawerPrimitive.Trigger;
+
+const DrawerClose = DrawerPrimitive.Close;
+
+const DrawerPortal = DrawerPrimitive.Portal;
+
+const DrawerOverlay = React.forwardRef(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Overlay
+    className={cn(
+      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      className,
+    )}
+    {...props}
+    ref={ref}
+  />
+));
+DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
+DrawerOverlay.propTypes = {
+  className: PropTypes.string,
+};
+
+const drawerVariants = cva(
+  'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out',
+  {
+    variants: {
+      side: {
+        top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
+        bottom:
+          'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
+        left: 'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
+        right:
+          'inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
+      },
+    },
+    defaultVariants: {
+      side: 'right',
+    },
+  },
+);
+
+const DrawerContent = React.forwardRef(({ side = 'right', className, children, ...props }, ref) => (
+  <DrawerPortal>
+    <DrawerOverlay />
+    <DrawerPrimitive.Content
+      ref={ref}
+      className={cn(drawerVariants({ side }), className)}
+      {...props}
+    >
+      {children}
+      <DrawerPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <Cross2Icon className="size-4" />
+        <span className="sr-only">Close</span>
+      </DrawerPrimitive.Close>
+    </DrawerPrimitive.Content>
+  </DrawerPortal>
+));
+DrawerContent.displayName = DrawerPrimitive.Content.displayName;
+DrawerContent.propTypes = {
+  side: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+const DrawerHeader = ({
+  className,
+  ...props
+}) => (
+  <div
+    className={cn(
+      'flex flex-col space-y-2 text-center sm:text-left',
+      className,
+    )}
+    {...props}
+  />
+);
+DrawerHeader.displayName = 'DrawerHeader';
+DrawerHeader.propTypes = {
+  className: PropTypes.string,
+};
+
+const DrawerFooter = ({
+  className,
+  ...props
+}) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className,
+    )}
+    {...props}
+  />
+);
+DrawerFooter.displayName = 'DrawerFooter';
+DrawerFooter.propTypes = {
+  className: PropTypes.string,
+};
+
+const DrawerTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Title
+    ref={ref}
+    className={cn('text-lg font-semibold text-foreground', className)}
+    {...props}
+  />
+));
+DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
+DrawerTitle.propTypes = {
+  className: PropTypes.string,
+};
+
+const DrawerDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Description
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props}
+  />
+));
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
+DrawerDescription.propTypes = {
+  className: PropTypes.string,
+};
+
+export {
+  Drawer,
+  DrawerPortal,
+  DrawerOverlay,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerDescription,
+};
