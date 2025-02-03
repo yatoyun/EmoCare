ccd59be48361f8e73ab9790d884016308a09bc6b
yatoyun <kaneko.yuta0813@gmail.com>
2025-02-03 18:35:52 +0900

    [frontend] add auth-layout

---
+++ frontend/src/components/layouts/auth-layout.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export const AuthLayout = ({ children, title }) => {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-3 text-center text-3xl font-extrabold text-gray-900">
+            {title}
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+AuthLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+};
