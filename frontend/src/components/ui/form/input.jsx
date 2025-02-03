65b7842e5857e5e0522cd9dab148dc7b4a331369
yatoyun <kaneko.yuta0813@gmail.com>
2025-02-03 18:34:40 +0900

    [frontend] add form

---
+++ frontend/src/components/ui/form/input.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '@/utils/cn';
+
+import { FieldWrapper } from './field-wrapper';
+
+const Input = React.forwardRef(
+  ({ className, type, label, error, registration, ...props }, ref) => {
+    return (
+      <FieldWrapper label={label} error={error}>
+        <input
+          type={type}
+          className={cn(
+            'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...registration}
+          {...props}
+        />
+      </FieldWrapper>
+    );
+  },
+);
+Input.displayName = 'Input';
+Input.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  error: PropTypes.string,
+  registration: PropTypes.object,
+};
+
+export { Input };
