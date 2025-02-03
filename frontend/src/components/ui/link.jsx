7f399955d3086c41c199d160e6fbae4a4e549d5e
yatoyun <kaneko.yuta0813@gmail.com>
2025-02-03 18:32:02 +0900

    [frontend] add link

---
+++ frontend/src/components/ui/link.jsx
@@ -0,0 +1,20 @@
+import { Link as RouterLink } from "react-router";
+import PropTypes from "prop-types";
+
+import { cn } from "@/utils/cn";
+
+export const Link = ({ className, children, ...props }) => {
+	return (
+		<RouterLink
+			className={cn("text-slate-600 hover:text-slate-900", className)}
+			{...props}
+		>
+			{children}
+		</RouterLink>
+	);
+};
+
+Link.propTypes = {
+	className: PropTypes.string,
+	children: PropTypes.node.isRequired,
+};
